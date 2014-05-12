fc60aed8ccc5271a2a66cb1e690e3958bb30857a
Victor Lin <victor@victorlinphoto.com>
2014-05-12 14:27:56 +0800

    Got template animations working

---
+++ client/views/application/layout.js
@@ -0,0 +1,11 @@
+Meteor.startup( function () {
+  $.getScript("assets/js/jquery.min.js");
+  $.getScript("assets/js/bootstrap.min.js");
+  $.getScript("assets/js/isotope.pkgd.min.js");
+  $.getScript("assets/js/imagesloaded.min.js");
+  $.getScript("assets/js/jquery.scrollTo.min.js");
+  $.getScript("assets/js/jquery.nav.min.js");
+  $.getScript("assets/js/jquery.appear.min.js");
+  $.getScript("assets/js/twitterFetcher.min.js");
+  $.getScript("assets/js/script.js");
+});
